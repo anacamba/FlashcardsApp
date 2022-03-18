0b0d68a98114a4f01fe4b5e553ddd626b5ab67fb
anacamba <ana.camba001@mymdc.net>
2022-03-18 11:39:47 -0400

    Ana's Flashcards app Lab #2

--- app/src/main/java/com/example/anacambaflashcards/MainActivity.java
+++ app/src/main/java/com/example/anacambaflashcards/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.anacambaflashcards;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
@@ -12,6 +14,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
 
         TextView flashcardQuestion = findViewById(R.id.flashcard_question_textview);
         TextView flashcardAnswer = findViewById(R.id.flashcard_answer_textview);
@@ -33,6 +37,36 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        findViewById(R.id.flashcard_add_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, AddCardActivity.class);
 
+                startActivityForResult(intent, 100);
+            }
+        });
+
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == 100) {
+            // get data
+            if (data != null) {
+
+               String questionString = data.getExtras().getString("QUESTION_KEY");
+                String answerString = data.getExtras().getString("ANSWER_KEY");
+                ((TextView)findViewById(R.id.flashcard_question_textview)).setText(questionString);
+                ((TextView)findViewById(R.id.flashcard_answer_textview)).setText(answerString);
+
+            }
+
+
+
+
+        }
     }
 }
